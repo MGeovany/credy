576d1388b2d06d0095fa92d59419f0d506401236
mgeovany <marlongeo1999@gmail.com>
2024-12-03 08:17:24 -0600

    Add Tailwind CSS configuration and integrate Rowdies font; update .gitignore and package.json

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        rowdies: ["Rowdies", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
